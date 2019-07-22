e28ac015a9629755d12bba260ee20938f217d552
Thomas Vigouroux <tomvig38@gmail.com>
2019-07-22 11:05:00 +0200

    More examples in playground

--- playground/test.c
+++ playground/test.c
@@ -1,2 +1,7 @@
+void TestWithParams(int param1, char param2) {}
 
-extern void test(int param1, char param2) {}
+void TestVoidParams(void) {}
+
+int TestNoParams() {}
+
+void TestDotParams(int a, int b, ...) {}
