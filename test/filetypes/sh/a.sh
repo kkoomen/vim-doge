e7db9278c77c3f9cd7d3f0d5ef9e7a3ea93689fa
kumarstack55 <kumarstack55@gmail.com>
2020-05-02 12:44:46 +0000

    Improve the regular expression of the sh function
    https://www.gnu.org/software/bash/manual/bash.html#Shell-Functions
    Functions are declared using this syntax:
    name () compound-command [ redirections ]
    or
    function name [()] compound-command [ redirections ]

---
+++ test/filetypes/sh/a.sh
@@ -0,0 +1,21 @@
+################################################################################
+# [TODO:description]
+# Globals:
+#   [TODO:var-name]
+# Arguments:
+#   $1: [TODO:description]
+# Returns:
+#   [TODO:description]
+################################################################################
+function test {
+  print "foobar"
+}
+
+test() {
+  print "foobar"
+}
+
+function test() {
+  print "foobar"
+}
+
