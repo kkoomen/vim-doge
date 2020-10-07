1f2b6a9d5871bee2432001668566322afd728776
Kim 金可明 <koomen@protonmail.com>
2020-10-07 14:19:44 +0800

    feat: run webpack build before running tests

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    libraryTarget: 'this',
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  externals: [nodeExternals()],
+};
