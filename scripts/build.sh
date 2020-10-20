42e92f620ade9a472b8d08b4a71701112b5447e9
Kim 金可明 <koomen@protonmail.com>
2020-10-20 23:36:24 +0800

    feat: archive binaries in builds for faster downloads

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,9 @@
 set -e
 set -u
 
-ROOT_DIR="$(cd "$(dirname "$0")"; pwd -P)/.."
+ROOT_DIR=$(realpath "$(cd "$(dirname "$0")"; pwd -P)/..")
 
+# Build the pkg lib prerequisites if needed.
 if [[ ! -d $ROOT_DIR/pkg/lib-es5 ]]; then
   cd $ROOT_DIR/pkg
   npm install --no-save && npm run prepare
@@ -14,6 +15,18 @@ fi
 
 cd $ROOT_DIR
 
+# Build the binaries.
 BUILD_TARGETS="node14-linux-x64,node14-macos-x64,node14-win-x64"
 node $ROOT_DIR/pkg/lib-es5/bin.js . -t "$BUILD_TARGETS" --out-path $ROOT_DIR/bin
 chmod +x $ROOT_DIR/bin/vim-doge*
+
+# Archive the binaries.
+cd $ROOT_DIR/bin
+rm -f $ROOT_DIR/bin/vim-doge-{linux,macos,win.exe}.tar.gz
+for file in ./vim-doge-{linux,macos,win.exe}; do
+  filename=$(basename "$file")
+  echo "==> Archiving $filename -> $filename.tar.gz"
+  tar -czf $filename.tar.gz "$filename" > /dev/null 2>&1
+done
+
+echo "🎉  Done building vim-doge binaries"
--- scripts/install.sh
+++ scripts/install.sh
@@ -10,7 +10,7 @@ if [[ -e ./bin/vim-doge ]]; then
 fi
 
 
-ROOT_DIR="$(cd "$(dirname "$0")"; pwd -P)/.."
+ROOT_DIR=$(realpath "$(cd "$(dirname "$0")"; pwd -P)/.."
 cd $ROOT_DIR
 mkdir ./bin
 
@@ -21,17 +21,21 @@ PKG_VERSION=$(cat "$ROOT_DIR/.version")
 RELEASE_URL="https://github.com/kkoomen/vim-doge/releases/download/$PKG_VERSION"
 
 if [[ $OS == 'Darwin' ]]; then
-  VIM_DOGE_EXECUTABLE_URL="$RELEASE_URL/vim-doge-macos"
+  TARGET="vim-doge-macos"
 elif [[ $OS == 'Linux' ]]; then
-  VIM_DOGE_EXECUTABLE_URL="$RELEASE_URL/vim-doge-linux"
+  TARGET="vim-doge-linux"
 else
-  VIM_DOGE_EXECUTABLE_URL="$RELEASE_URL/vim-doge-win.exe"
+  TARGET="vim-doge-win.exe"
   OUTFILE="$OUTFILE.exe"
 fi
 
-echo "Downloading $VIM_DOGE_EXECUTABLE_URL"
+TEMP_FILE="vim-doge.tar.gz"
+DOWNLOAD_URL="$RELEASE_URL/$TARGET.tar.gz"
+echo "Downloading $DOWNLOAD_URL"
 curl -L --progress-bar \
     --fail \
-    --output "$OUTFILE" \
-    $VIM_DOGE_EXECUTABLE_URL
-chmod +x $OUTFILE
+    --output "$TEMP_FILE" \
+    "$DOWNLOAD_URL"
+tar xzf "$TEMP_FILE" && mv "$TARGET" "$OUTFILE"
+rm -f "$TEMP_FILE"
+chmod +x "$OUTFILE"
