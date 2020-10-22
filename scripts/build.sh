0c35ac97ea21f7005b54de58b512a4acebc11145
Kim 金可明 <koomen@protonmail.com>
2020-10-22 21:27:34 +0800

    feat: remove travis.yml and setup initial CI workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,7 @@ set -e
 set -u
 
 ROOT_DIR=$(cd "$(dirname "$0")/.."; pwd -P)
+TARGET="${1:-}"
 
 # Build the pkg lib prerequisites if needed.
 if [[ ! -d $ROOT_DIR/pkg/lib-es5 ]]; then
@@ -16,17 +17,10 @@ fi
 cd $ROOT_DIR
 
 # Build the binaries.
-BUILD_TARGETS="node14-linux-x64,node14-macos-x64,node14-win-x64"
+if [[ "$TARGET" != "" ]]; then
+  BUILD_TARGETS="$TARGET"
+else
+  BUILD_TARGETS="node14-linux-x64,node14-macos-x64,node14-win-x64"
+fi
 node $ROOT_DIR/pkg/lib-es5/bin.js . -t "$BUILD_TARGETS" --out-path $ROOT_DIR/bin
 chmod +x $ROOT_DIR/bin/vim-doge*
-
-# Archive the binaries.
-cd $ROOT_DIR/bin
-rm -f ./vim-doge-{linux,macos,win.exe}.tar.gz
-for file in ./vim-doge-{linux,macos,win.exe}; do
-  filename=$(basename "$file")
-  echo "==> Archiving $filename -> $filename.tar.gz"
-  tar -czf $filename.tar.gz "$filename" > /dev/null 2>&1
-done
-
-echo "🎉  Done building vim-doge binaries"
