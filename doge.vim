3c145fd343e95099d4c723db7bc8f98d18c9a790
Kim 金可明 <koomen@protonmail.com>
2021-03-07 21:25:39 +0800

    feat: replace vercel/pkg with caxa

---
+++ doge.vim
@@ -0,0 +1,5 @@
+set nocompatible
+set runtimepath^=/Users/jinkeming/.vim/plugged/vim-doge
+filetype plugin indent on
+syntax on
+set hidden
--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,23 +1,23 @@
 param([String] $buildTarget, [String] $outFile)
 $rootDir = Resolve-Path -Path ((Split-Path $myInvocation.MyCommand.Path) + "\..")
 
-# Build the pkg lib prerequisites if needed.
-if (!(Test-Path "$rootDir\pkg\lib-es5")) {
-  cd $rootDir\pkg
-  npm install --no-save; if ($?) {npm run prepare}
+if (!(Test-Path "$rootDir\bin")) {
+  New-Item -Path "$rootDir\bin" -ItemType Directory
 }
 
 cd $rootDir
 
 # Build the binary.
-node "$rootDir\pkg\lib-es5\bin.js" . -t "$buildTarget" --out-path "$rootDir\bin"
+if ($outFile -eq "") {
+  $rootDir\node_modules\.bin\caxa --directory "$rootDir/build" --command "{{caxa}}/node_modules/.bin/node" "{{caxa}}/index.js" --output "./bin/vim-doge.exe"
+} else {
+  $rootDir\node_modules\.bin\caxa --directory "$rootDir/build" --command "{{caxa}}/node_modules/.bin/node" "{{caxa}}/index.js" --output "./bin/$outFile.exe"
+}
 
 # Archive the binary.
-if ($outFile -ne "") {
-  $outFile = "$rootDir\bin\$outFile.zip"
-  rm $rootDir\bin\*.zip
-  echo "==> Archiving $rootDir\bin\vim-doge.exe -> $outFile"
-  7z a -tzip "$outFile" "$rootDir\bin\vim-doge.exe"
-}
+$outFile = "$rootDir\bin\$outFile.zip"
+rm $rootDir\bin\*.zip
+echo "==> Archiving $rootDir\bin\vim-doge.exe -> $outFile"
+7z a -tzip "$outFile" "$rootDir\bin\vim-doge.exe"
 
 echo "Done building vim-doge binaries"
