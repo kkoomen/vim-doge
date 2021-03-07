cd8b9fb515befdd209b6a20d247bb132bdf7ca4c
Kim 金可明 <koomen@protonmail.com>
2021-03-07 22:54:14 +0800

    fix: remove prettier/@typescript-eslint from eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint',
   ],
   root: true,
   env: {
