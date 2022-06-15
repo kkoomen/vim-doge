742fd25d9432d687a242d5933e23c0a4f55a8031
Kim 金可明 <koomen@protonmail.com>
2022-06-15 09:32:01 +0200

    feat: expand comment where the function starts

--- src/parsers/cpp.service.ts
+++ src/parsers/cpp.service.ts
@@ -27,6 +27,10 @@ export class CppParserService extends BaseParserService implements CustomParserS
       this.nodeTypes.includes(node.type) &&
       this.done === false
     ) {
+      this.setDefaultResult({
+        startPosition: node.startPosition.row,
+      });
+
       switch (node.type) {
         case NodeType.TEMPLATE_DECLARATION: {
           const childNode: SyntaxNode = node.children
