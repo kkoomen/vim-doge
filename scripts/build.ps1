b56c775d1accea1eeddf6ed49c6a0c6514972b3d
Kim 金可明 <koomen@protonmail.com>
2023-07-09 13:03:16 +0200

    fix(build.ps1): add .exe everywhere

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,17 +4,17 @@ $OutFile = $args[1]
 
 Set-Location $RootDir
 if (!(Test-Path -Path "./bin")) { New-Item -ItemType Directory -Path "./bin" | Out-Null }
-if (Test-Path -Path "./bin/vim-doge-helper") { Remove-Item -Path "./bin/vim-doge-helper" }
+if (Test-Path -Path "./bin/vim-doge-helper.exe") { Remove-Item -Path "./bin/vim-doge-helper.exe" }
 
 # Build the binary.
 Set-Location "$RootDir/helper"
 if ($BuildTarget -ne "") {
   cargo build --release --target $BuildTarget
-  Copy-Item -Path "target/$BuildTarget/release/vim-doge-helper" -Destination "../bin/"
+  Copy-Item -Path "target/$BuildTarget/release/vim-doge-helper.exe" -Destination "../bin/"
 }
 else {
   cargo build --release
-  Copy-Item -Path "target/release/vim-doge-helper" -Destination "../bin/"
+  Copy-Item -Path "target/release/vim-doge-helper.exe" -Destination "../bin/"
 }
 
 # Archive the binary.
@@ -23,8 +23,8 @@ if ($OutFile -ne "") {
 
   Set-Location "$RootDir/bin"
   Remove-Item -Path "./*.zip"
-  Write-Output "==> Archiving $RootDir/bin/vim-doge-helper -> $RootDir/bin/$OutFile"
-  7z a -tzip "$OutFile" vim-doge-helper
+  Write-Output "==> Archiving $RootDir/bin/vim-doge-helper.exe -> $RootDir/bin/$OutFile"
+  7z a -tzip "$OutFile" "vim-doge-helper.exe"
 }
 
 Write-Output "🎉  Done building vim-doge-helper"
