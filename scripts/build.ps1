dc5ccf99ae320238f1e0972c38cac3a58531a5fa
Kim 金可明 <koomen@protonmail.com>
2023-07-09 14:04:33 +0200

    chore(ci): enable all tests for vim 8 and head

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -6,34 +6,6 @@ Set-Location $RootDir
 if (!(Test-Path -Path "./bin")) { New-Item -ItemType Directory -Path "./bin" | Out-Null }
 if (Test-Path -Path "./bin/vim-doge-helper.exe") { Remove-Item -Path "./bin/vim-doge-helper.exe" }
 
-function Get-DirectoryTree {
-    param(
-        [string]$Path = (Get-Location)
-    )
-
-    # Recursive function to get the subdirectories
-    function Get-Subdirectories {
-        param([string]$Path, [int]$IndentLevel)
-
-        $indentation = '  ' * $IndentLevel
-        $subdirectories = Get-ChildItem -Path $Path -Directory
-
-        foreach ($subdir in $subdirectories) {
-            Write-Host "$indentation$subdir"
-            Get-Subdirectories -Path $subdir.FullName -IndentLevel ($IndentLevel + 1)
-        }
-    }
-
-    # Main script logic
-    if (Test-Path -Path $Path -PathType Container) {
-        Write-Host $Path
-        Get-Subdirectories -Path $Path -IndentLevel 1
-    }
-    else {
-        Write-Host "Invalid directory path."
-    }
-}
-
 # Build the binary.
 Set-Location "$RootDir/helper"
 
@@ -43,7 +15,6 @@ if ($BuildTarget) {
 }
 else {
   cargo build --release
-  Get-DirectoryTree
   Copy-Item -Path "target/release/vim-doge-helper.exe" -Destination "../bin/"
 }
 
