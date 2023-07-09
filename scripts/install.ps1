e9ecb4bd9ac7f94d5a6e8a3085bc3453c2b9d876
Kim 金可明 <koomen@protonmail.com>
2023-07-09 15:17:28 +0200

    fix(install.ps1): remove typo and make syntax more consistent

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,32 +1,36 @@
-echo "Preparing to download vim-doge-helper binary..."
+Write-Output "[vim-doge] Preparing to download vim-doge-helper binary..."
 
 $Arch = $env:PROCESSOR_ARCHITECTURE
 if ($Arch -eq 'x86') {
-  # 32-bit architecture
+  # 32-bit
   $AssetName = "vim-doge-helper-windows-i686.zip"
 }
 else {
-  # 64-bit architecture
+  # 64-bit
   $AssetName = "vim-doge-helper-windows-x86_64.zip"
 }
 
+$RootDir = Resolve-Path -Path ((Split-Path $myInvocation.MyCommand.Path) + "\..")
+$AppVersion = Get-Content "$RootDir\.version"
 
-$RootDir. = Resolve-Path -Path ((Split-Path $myInvocation.MyCommand.Path) + "\..")
-$AppVersion = Get-Content "$RootDir.\.version"
-
-$AssetPath = "$RootDir.\bin\$AssetName"
-$OutFile = "$RootDir.\bin\vim-doge-helper.exe"
+$AssetPath = "$RootDir\bin\$AssetName"
+$OutFile = "$RootDir\bin\vim-doge-helper.exe"
 
 $DownloadUrl = "https://github.com/kkoomen/vim-doge/releases/download/v$AppVersion/$AssetName"
 
 if (Test-Path $AssetName) {
-  rm "$AssetName"
+  Remove-Item "$AssetName"
 }
 
 if (Test-Path $OutFile) {
-  rm "$OutFile"
+  Remove-Item "$OutFile"
 }
 
-Invoke-WebRequest -uri $DownloadUrl -OutFile ( New-Item -Path "$AssetPath" -Force )
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+
+Invoke-WebRequest -Uri $DownloadUrl -OutFile ( New-Item -Path "$AssetPath" -Force )
 Expand-Archive -LiteralPath "$AssetPath" -DestinationPath "$RootDir.\bin"
-rm "$AssetPath"
+Remove-Item "$AssetPath"
+
+Write-Output "[vim-doge] Successfully downloaded vim-doge-helper"
